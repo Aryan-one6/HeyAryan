da9034b44eb8a4c98093696a7db119dd84af63ad
Aryan-one6 <parashar.one6@gmail.com>
2024-07-27 02:59:50 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
-
+import { Analytics } from "@vercel/analytics/react"
 const App = () => {
 
   return (
@@ -9,6 +9,7 @@ const App = () => {
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
+          <Analytics/>
           <Hero />
 
         </div>
