0988391a50c7ff56ada421e3de0953733356e951
Aryan-one6 <parashar.one6@gmail.com>
2024-08-18 22:43:08 +0530

    changed technologies style

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -9,6 +9,9 @@ import { SectionWrapper } from "../hoc";
 const Tech = () => {
   const [clickedTech, setClickedTech] = useState(null);
 
+  // Function to determine if the device is mobile
+  const isMobile = window.innerWidth <= 768;
+
   return (
     <div id="tech">
       <motion.div variants={textVariant()} className="text-center">
@@ -27,10 +30,16 @@ const Tech = () => {
             className={`w-28 h-28 flex justify-center items-center rounded-full bg-transparent p-5 transition-transform duration-300 ${
               clickedTech === technology.name ? "scale-110" : ""
             }`}
-            whileHover={{ rotate: 720, scale: 1.1, boxShadow: "0px 0px 40px rgba(145, 94, 255, 1)" }} // Spin and increase glow on hover
+            whileHover={{ 
+              rotate: 720, 
+              scale: 1.1, 
+              boxShadow: isMobile ? "0px 0px 60px rgba(145, 94, 255, 1)" : "0px 0px 40px rgba(145, 94, 255, 1)" 
+            }} // Spin and increase glow on hover
             onClick={() => setClickedTech(technology.name)} // Apply scale on click for mobile
-            style={{ boxShadow: "0px 0px 10px rgba(145, 94, 255, 0.3)" }} // Reduced initial light effect
-            transition={{ duration: 1 }} // Spin over 1 second
+            style={{ 
+              boxShadow: "0px 0px 10px rgba(145, 94, 255, 0.3)", 
+              transition: isMobile ? "transform 2s, box-shadow 1.5s" : "transform 1s, box-shadow 1s" 
+            }} // Adjust transition for mobile
           >
             <img src={technology.icon} alt={technology.name} className="w-16 h-16 object-contain" />
           </motion.div>
@@ -41,7 +50,6 @@ const Tech = () => {
 };
 
 export default SectionWrapper(Tech, "");
-
 
 
 
--- src/styles.js
+++ src/styles.js
@@ -9,7 +9,7 @@ const styles = {
     "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[15px] text-[15px] lg:leading-[40px]",
 
   sectionHeadText:
-    "text-white font-black md:text-[60px] sm:text-[30px] xs:text-[30px] text-[40px]",
+    "text-white font-black md:text-[60px] sm:text-[25px] xs:text-[25px] text-[40px]",
   sectionSubText:
     "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
 };
